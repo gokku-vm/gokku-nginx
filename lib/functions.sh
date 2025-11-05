453455fe6e083b5a770dbe1e3e0013768144cb26
thadeu <tadeuu@gmail.com>
2025-11-05 16:38:54 -0300

    fix: try change proxy_x_timeout fields

--- lib/functions.sh
+++ lib/functions.sh
@@ -115,9 +115,9 @@ generate_locations_block() {
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_connect_timeout 10s;
-        proxy_send_timeout 60s;
-        proxy_read_timeout 60s;
+        proxy_connect_timeout 30s;
+        proxy_send_timeout 0s;
+        proxy_read_timeout 0s;
         proxy_buffering off;
         proxy_http_version 1.1;
         proxy_set_header Connection "";
