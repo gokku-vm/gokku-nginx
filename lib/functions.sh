12febba84e6cc5191f893025392804309a437074
thadeu <tadeuu@gmail.com>
2025-11-11 13:46:01 -0300

    change proxy_read_timeout to 60s

--- lib/functions.sh
+++ lib/functions.sh
@@ -116,8 +116,8 @@ generate_locations_block() {
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
         proxy_connect_timeout 30s;
-        proxy_send_timeout 0s;
-        proxy_read_timeout 0s;
+        proxy_send_timeout 60s;
+        proxy_read_timeout 60s;
         proxy_buffering off;
         proxy_http_version 1.1;
         proxy_set_header Connection "";
